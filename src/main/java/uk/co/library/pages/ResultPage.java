769da175e59c4933a55631b4944e3310b27f1233
NP214 <nidhiprime214@gmail.com>
2022-05-10 14:47:34 -0700

    cv lib

---
+++ src/main/java/uk/co/library/pages/ResultPage.java
@@ -0,0 +1,17 @@
+package uk.co.library.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import uk.co.library.utility.Utility;
+
+public class ResultPage extends Utility {
+    public ResultPage(){ PageFactory.initElements(driver,this);}
+
+    @FindBy(xpath = "/html[1]/body[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/h1[1]")
+    WebElement resultText;
+
+    public String verifyText(String result){
+        return getTextFromElement(resultText);
+    }
+}
